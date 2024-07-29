b42f8ebf5d042693c2fd5f87f3d1b7e98f9a6a0d
KaloinaRandria <kaloinarandria@outlook.fr>
2024-07-29 14:48:59 +0300

    File P4

---
+++ P4.py
@@ -0,0 +1,7 @@
+# P4:
+# Considérons la suite de Fibonnaci (Fn) nE IN
+# Fn =1 si n=0 ou n=1
+# Fn =Fn-1 + Fn-2 si n>=2
+# 	Ex: 1,1,2,3,5,8,13,21,34,55,89,144
+# 	F11 est le premier terme de la suite qui contient 3 chiffres.
+# Trouver le plus petit n tq Fn est le premier terme contenant 1000 chiffres.
