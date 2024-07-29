01f1dd256f8ba04b1c8aa26685e6673bffa2d7d8
KaloinaRandria <kaloinarandria@outlook.fr>
2024-07-29 16:23:45 +0300

    iterative method

--- P4.py
+++ P4.py
@@ -5,18 +5,30 @@
 # 	Ex: 1,1,2,3,5,8,13,21,34,55,89,144
 # 	F11 est le premier terme de la suite qui contient 3 chiffres.
 # Trouver le plus petit n tq Fn est le premier terme contenant 1000 chiffres.
-def fibonnaci(nombre):
+def fibonnaciRecursive(nombre):
     if nombre < 1 :
         return nombre
-    return abs(fibonnaci(nombre-1) + fibonnaci(nombre-2))
+    return abs(fibonnaciRecursive(nombre-1) + fibonnaciRecursive(nombre-2))
+
+def fibonacciIterative(n):
+    """Retourne le n-ième nombre de Fibonacci de manière itérative."""
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
 
 def searchN(count):
     n = 1
     while True:
-        fn = fibonnaci(n)
+        fn = fibonacciIterative(n)
         if count == len(str(fn)):
             return fn
         n += 1
 
-print (searchN(10))
+print (searchN(1000))
 
