6f277ec32bc5d2bb0d0661e4d18f8ae17ab3f33c
KaloinaRandria <kaloinarandria@outlook.fr>
2024-07-29 17:03:39 +0300

    add P6

---
+++ P6.py
@@ -0,0 +1,4 @@
+# P6:
+# Un nombre premier est appelé un nombre premier circulaire si toutes rotations possibles de ces chiffres sont premiers.
+#  	Ex: 197 est un premier circulaire car 719 et 971 est premier.
+# Trouver tout les nombres premiers inférieurs a 1000000.
