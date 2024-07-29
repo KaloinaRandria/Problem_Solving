bd64f78ab3ba5609595fe05da20326d62eba7871
KaloinaRandria <kaloinarandria@outlook.fr>
2024-07-29 18:45:33 +0300

    rotate number

--- P6.py
+++ P6.py
@@ -2,3 +2,26 @@
 # Un nombre premier est appelé un nombre premier circulaire si toutes rotations possibles de ces chiffres sont premiers.
 #  	Ex: 197 est un premier circulaire car 719 et 971 est premier.
 # Trouver tout les nombres premiers inférieurs a 1000000.
+def estPremier(nombre):
+    if nombre <= 1:
+        return False
+    if nombre <= 3:
+        return True
+    if nombre % 2 == 0 or nombre % 3 == 0:
+        return False
+    i = 5
+    while i*i <= nombre:
+        if nombre % i == 0 or nombre % (i+2) == 0:
+            return False
+        i += 6
+    return True
+
+def rotateNumber(nombre):
+    valiny = []
+    n = str(nombre)
+    for i in range(len(n)):
+        rotation = n[i:] + n[:i]
+        valiny.append(int(rotation))
+    return valiny
+
+print(rotateNumber(197))
